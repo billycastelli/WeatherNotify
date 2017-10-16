dc547602be038c96be5bf33aa410bfee24ec7d7c
William Castelli (Billy) <billycastelli@gmail.com>
2017-10-16 10:56:43 -0700

    Update wuInput.py

--- wuInput.py
+++ wuInput.py
@@ -1,5 +1,7 @@
 #Weather input
+
 import wuApi
+import wuTw
 
 def getCity():
     city = input('Enter the name of a US city: ')
@@ -11,24 +13,36 @@ def getState():
 
 def getPhone():
     phone = ''
-    while len(phone) != 10:
-        phone = input('Enter phone number: ')
+    #while len(phone) != 10:
+    #for now only my phone number will work because this program
+    #uses the twilio free trial
+    phone = input('Enter phone number (10 digits, include 1): ')
     return phone
 
 if __name__ == "__main__":
     city = getCity()
     state = getState()
-    #phone = getPhone()
+    phone = getPhone()
+    twNum = '+18589265834'
+
     url = wuApi.build_url(city,state)
+    print(url)
     #url = wuApi.build_url('carlsbad','ca')
     json = wuApi.get_result(url)
     weatherDict = wuApi.parse(json)
     print()
+    #print(weatherDict)
     for date in weatherDict.keys():
-        print(date)
-        print(weatherDict[date])
-        print()
+        conditions, high, low = weatherDict[date]
+        todayDate = '\n \nToday is ' + date + '\n'
+        todayW = "The weather today will be: "+ weatherDict[date][0]
+        todayHiLow = "\nThe high will be " + high +" degrees. The low will be " + low + " degrees."
+        #print(date)
+        #print(weatherDict[date])
+        #print()
+    wuTw.send(phone, twNum, todayDate + todayW + todayHiLow)
+    
     #print(json)
 
-    
+   
     
