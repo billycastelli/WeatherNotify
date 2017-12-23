aa6bfeb118fc7be651968119dd720dde969a029b
William Castelli (Billy) <billycastelli@gmail.com>
2017-12-22 18:22:21 -0800

    Update wuServer
    Correct date displayed using datetime

--- wuServer.py
+++ wuServer.py
@@ -3,12 +3,17 @@
 import wuApi
 import wuTw
 from apscheduler.schedulers.blocking import BlockingScheduler
-
+from datetime import date
 
 city = 'Irvine'
 state = 'California'
 phone = '17607077076'
-twNum = '+18589265834'
+twNum = '+17609744094'
+
+def today(year, month, day):
+    string = ""
+    string = month + " " + str(day) + ", " + str(year)
+    return string
 
 def job():
     url = wuApi.build_url(city,state)
@@ -18,23 +23,14 @@ def job():
     weatherDict = wuApi.parse(json)
     print()
     #print(weatherDict)
-    for date in weatherDict.keys():
-        conditions, high, low = weatherDict[date]
-        todayDate = '\n \nToday is ' + date + '\n'
-        todayW = "The weather today will be: "+ weatherDict[date][0]
-        todayHiLow = "\nThe high will be " + high +" degrees. The low will be " + low + " degrees."
-        break
-    wuTw.send(phone, twNum, todayDate + todayW + todayHiLow)
-
-
+    todayDate = today(date.today().year, date.today().strftime("%B"), date.today().day)
+    conditions, high, low = weatherDict[todayDate]
+    dateMessage = '\n \nToday is ' + todayDate + '\n'
+    weatherMessage = "The weather today will be: "+ weatherDict[todayDate][0]
+    tempsMessage = "\nThe high will be " + high +" degrees. The low will be " + low + " degrees."    
+    wuTw.send(phone, twNum, dateMessage + weatherMessage + tempsMessage)
 
 if __name__ == "__main__":
     scheduler = BlockingScheduler()
-    scheduler.add_job(job, 'cron', hour = 8, minute = 30)
+    scheduler.add_job(job, 'cron', hour = 15, minute = 52)
     scheduler.start()
-
-    
-
-    
-
-    
